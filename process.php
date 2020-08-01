5f7f9da3a28695d6550ccfd726dace49ca535cd8
luke-h1 <luke.howsam@yahoo.com>
2020-08-01 18:39:20 +0100

    final commit

---
+++ process.php
@@ -0,0 +1,19 @@
+<?php 
+
+echo 'processing...' ;   
+
+
+
+// check for GET variable  
+
+if(isset($_GET['name'])){ 
+  echo 'GET: Your name is '. $_GET['name']; 
+}
+
+
+// connect to db 
+
+
+
+
+?> 
